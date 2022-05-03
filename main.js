37a0d993bc7ec04f330685fe100a6c24117ca21e
Yorch82 <jcampo1982@outlook.com>
2022-05-03 15:43:40 +0200

    finalizado

--- main.js
+++ main.js
@@ -71,3 +71,70 @@ countTheArgs('gato', 'perro'); //2
 countTheArgs('gato', 'perro', 'pollo', 'oso'); //4
 
 // Escribe una función llamada combineTwoArrays que reciba dos array cómo argumentos y devuelva solo un array que combine los dos (usando spread operator).
+
+comida = ['patata', 'coche']
+bebida = ['tomate']
+
+const combineTwoArrays = (com, beb) => {
+    let arrayCombinada = [...com, ...beb]
+    console.log(arrayCombinada)
+}
+combineTwoArrays (comida, bebida)
+
+// 3. Extras
+
+const HIGH_TEMPERATURES = {
+    yesterday: 30,
+    today: 35,
+    tomorrow: 32,
+};
+
+
+// Cambiar las siguientes líneas para guardar desestructurando los valores de temperaturas en las variables maximaHoy y maximaManana
+
+// const maximaHoy = HIGH_TEMPERATURES.today;
+// const maximaManana = HIGH_TEMPERATURES.tomorrow;
+const {today:maximaHoy, tomorrow:maximaManana } = HIGH_TEMPERATURES
+
+console.log(maximaHoy)
+console.log(maximaManana)
+
+
+// Escriba una función llamada onlyUniques que acepte cualquier número de argumentos y devuelva un array de elementos únicos, sin repetidos.
+
+const onlyUniques = (...args) => {
+    let onlyArray = [...new Set(args)]
+    console.log(onlyArray)
+}
+
+onlyUniques('gato', 'pollo', 'cerdo', 'cerdo');//['gato', 'pollo', 'cerdo']
+onlyUniques(1, 1, 2, 2, 3, 6, 7, 8); //[1, 2, 3, 6, 7, 8]
+
+// Escriba una función llamada combineAllArrays que pueda recibir cualquier cantidad de arrays como argumentos y los combine todos en un solo array.
+
+const combineAllArrays = (...variasArrays) => {
+    let unicArray = []
+    variasArrays.forEach (singleArray => {
+        unicArray = unicArray.concat(singleArray)
+    })
+    console.log(unicArray)
+}
+
+combineAllArrays([3, 6, 7, 8],[2, 7, 3, 1])
+// [3, 6, 7, 8, 2, 7, 3, 1]
+combineAllArrays([2, 7, 3, 1],[2, 7, 4, 12],[2, 44, 22, 7, 3, 1]);
+// [2, 7, 3, 1, 2, 7, 4, 12, 2, 44, 22, 7, 3, 1]
+
+// Escriba una función llamada sumAndSquare que reciba cualquier número de argumentos, los eleve al cuadrado y devuelva la suma de todos los valores cuadrados.
+
+const exponente = 2;
+
+const sumAndSquare = (...anyArgument) => {
+    let counter = 0
+    anyArgument.forEach (argu => {
+        counter += Math.pow(argu, exponente)        
+    })
+    console.log(counter)
+}
+
+sumAndSquare (2, 2, 3)
